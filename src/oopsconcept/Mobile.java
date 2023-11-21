be67d7691c5627cc8f44b6f3de618bea7b902da1
biplab-w <waglebip@gmail.com>
2023-11-20 22:49:16 -0600

    Added Mobile.java

--- src/oopsconcept/Mobile.java
+++ src/oopsconcept/Mobile.java
@@ -1,5 +1,18 @@
 package oopsconcept;
 
 public class Mobile {
+        // State
+        // Make
+        // Model
+        // Storage
+        // Ram size
+        // OS
+        // Display
+        // Camera
+        //
+
+
+        //Behavior
+
 
 }
